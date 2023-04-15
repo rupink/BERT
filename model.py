d572b518e67d8d6ed72178965ae40c699702f2b6
rupink <66811124+rupink@users.noreply.github.com>
2023-04-14 23:23:38 -0400

    Update model.py

--- model.py
+++ model.py
@@ -22,8 +22,21 @@ model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=2e-5),
               metrics=['accuracy'])
 
 # Load the data
-train_text = [...] # List of training texts
-train_labels = [...] # List of training labels
+train_text = [    "I loved the movie, it was amazing!",    "The acting was terrible, I couldn't even finish the movie",    "The plot was too predictable, I expected more",    "The special effects were mind-blowing, I was on the edge of my seat",    "The characters were flat and uninteresting, I didn't care what happened to them",    "The dialogue was witty and engaging, I laughed out loud several times",    "The pacing was slow and boring, I kept checking my phone",    "The cinematography was stunning, every shot was a work of art",    "The soundtrack was distracting and didn't fit the mood of the movie",    "The ending was satisfying and tied up all the loose ends",    "The movie was a complete waste of time, I regret watching it",    "The movie was an instant classic, I will definitely watch it again"] # List of training texts
+train_labels = [
+    1,  # Positive
+    0,  # Negative
+    0,  # Negative
+    1,  # Positive
+    0,  # Negative
+    1,  # Positive
+    0,  # Negative
+    1,  # Positive
+    0,  # Negative
+    1,  # Positive
+    0,  # Negative
+    1   # Positive
+] # List of training labels
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
 train_encoded = tokenizer(train_text, padding=True, truncation=True, max_length=max_length, return_tensors='tf')
 train_dataset = tf.data.Dataset.from_tensor_slices((dict(train_encoded), train_labels)).shuffle(len(train_labels)).batch(32)
