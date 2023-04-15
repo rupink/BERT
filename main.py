e7b916f8e7bd309c36f42ba387c0e3174d10a25d
rupink <66811124+rupink@users.noreply.github.com>
2023-04-14 23:23:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,21 @@ from model import model, train_dataset
 from hyperparam_tuning import grid_search
 from app import app
 
-train_text = [...] # List of training texts
-train_labels = [...] # List of training labels
+train_text = [    "I loved the movie, it was amazing!",    "The acting was terrible, I couldn't even finish the movie",    "The plot was too predictable, I expected more",    "The special effects were mind-blowing, I was on the edge of my seat",    "The characters were flat and uninteresting, I didn't care what happened to them",    "The dialogue was witty and engaging, I laughed out loud several times",    "The pacing was slow and boring, I kept checking my phone",    "The cinematography was stunning, every shot was a work of art",    "The soundtrack was distracting and didn't fit the mood of the movie",    "The ending was satisfying and tied up all the loose ends",    "The movie was a complete waste of time, I regret watching it",    "The movie was an instant classic, I will definitely watch it again"] # List of training texts
+train_labels = [
+    1,  # Positive
+    0,  # Negative
+    0,  # Negative
+    1,  # Positive
+    0,  # Negative
+    1,  # Positive
+    0,  # Negative
+    1,  # Positive
+    0,  # Negative
+    1,  # Positive
+    0,  # Negative
+    1   # Positive
+] # List of training labels
 
 train_text = [clean_text(text) for text in train_text]
 
